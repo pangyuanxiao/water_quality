b776715d30d85c958df68bd40e9da263b759874c
xiadi <1>
2023-07-11 20:39:19 +0800

    增加表格

--- src/views/InfoPlatform.vue
+++ src/views/InfoPlatform.vue
@@ -1,21 +1,83 @@
 <template>
   <div class="back-pc">
-     <div id="container"></div>
+  <a-form
+    layout="inline"
+    style="padding: 50px;"
+  >
+    <a-form-item label="Type:">
+      
+    </a-form-item>
+  </a-form>
+  <a-table :columns="columns" :data-source="data"  bordered style="padding: 5vw;"  > 
+    <template #bodyCell="{ column, text }">
+      <template v-if="column.dataIndex === 'name'">
+        <a>{{ text }}</a>
+      </template>
+    </template>
+  </a-table>
   </div>
  
 </template>
-
 <script lang="ts">
 import { defineComponent } from 'vue';
-import {demo} from './graph'
-export default defineComponent({
-  mounted() {
-     demo();
-      //test();
-  },
-})
-</script>
 
-function mounted() {
-  throw new Error('Function not implemented.');
+const columns = [
+  {
+    title: 'Index',
+    dataIndex: 'Index',
+  },
+  {
+    title: 'Date',
+    dataIndex: 'Date',
+  },
+  {
+    title: 'Theme',
+    dataIndex: 'Theme',
+  },
+  {
+    title: 'Type',
+    dataIndex: 'Type',
+  },
+  {
+    title: 'Area',
+    dataIndex: 'Area',
+  },
+  {
+    title: 'Describe',
+    dataIndex: 'Describe',
+  },
+  {
+    title: 'Contact information',
+    dataIndex: 'Contact',
+  },
+];
+
+const data = [
+  {
+    Index: '1',
+    Date: '2023/06/20',
+    Theme: 'Low oxygen content',
+    Type: 'technical support',
+    Area: 'Chaohu Lake',
+    Describe: 'Low oxy',
+    Contact :'+86 152xxxx2828'
+    
+  },
+  
+];
+
+export default defineComponent({
+  setup() {
+    return {
+      data,
+      columns,
+    };
+  },
+});
+</script>
+<style>
+th.column-money,
+td.column-money {
+  text-align: right !important;
 }
+</style>
